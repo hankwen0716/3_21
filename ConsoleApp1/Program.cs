b6b33d2fef5da8b621f0becb930230e877e634fd
user <user@DESKTOP-6JBEF5K>
2022-03-21 10:04:40 +0800

    自建類別、多型測試

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,11 +7,48 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            Vehicle Myveh = new Vehicle();
 
+            Myveh.lunch();
+            Myveh.info();
 
-
-
+            Veh_b Myveh_b = new Veh_b();
+            Myveh_b.info();
             //Console.WriteLine("Hello World!");
+
+            /* static void add_function(int a,int b)
+            {
+
+
+
+
+                return ;
+            } */
+        }
+
+    }
+    class Vehicle
+    {
+        int cc = 150;
+        string name = "no name";
+        public void lunch()
+        {
+            Console.WriteLine("Lunching....");
+        }
+        public virtual void info()
+        {
+            Console.WriteLine("name:"+name);
+            Console.WriteLine("CC:"+cc);
+            Console.WriteLine(".........");
+        }
+
+
+    }
+    class Veh_b : Vehicle
+    {
+        public override void info()
+        {
+            Console.WriteLine("veh_b.info");
         }
     }
 }
